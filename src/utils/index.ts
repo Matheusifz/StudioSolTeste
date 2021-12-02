60a9e5ea7091c6eec62f82b9b2ab4d655e95f054
matheusifz <matheusifz@gmail.com>
2021-12-02 18:55:42 -0300

    fix: improve compareValue function security

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,9 +1,11 @@
-export const compareValues = (firstNumber:number, secondNumber:number) => {
+export const compareValues = (firstValue: number, secondValue: number) => {
+  const firstNumber = Number(firstValue);
+  const secondNumber = Number(secondValue);
   if (firstNumber === secondNumber) {
-    return "Equal"
+    return "Equal";
   } else if (firstNumber < secondNumber) {
-    return "Smaller"
-  } else {
-    return "Bigger"
-  }
+    return "Smaller";
+  } else if (firstNumber > secondNumber) {
+    return "Bigger";
+  } else return "Error";
 };
