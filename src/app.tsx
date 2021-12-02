473a6be19b46a6982a27761eb3ad68ecf90a2e82
matheusifz <matheusifz@gmail.com>
2021-12-02 18:28:16 -0300

    feat: add components to app

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,13 @@
 import { useEffect, useState } from "react";
+import { Button } from "./components/Button";
+import Input from "./components/Input";
 import { fetchNumber } from "./services";
+import { compareValues } from "./utils";
 
 const App = () => {
-  const [number, setNumber] = useState(null);
+  const [number, setNumber] = useState(0);
+  const [userInputValue, setUserInputValue] = useState(0);
+
   useEffect(() => {
     const getNumber = async () => {
       const number = await fetchNumber();
@@ -10,7 +15,30 @@ const App = () => {
     };
     getNumber();
   }, []);
-  return <h1>{number}</h1>;
+
+  const fetchNewNumber = async () => {
+    const number = await fetchNumber();
+    setNumber(number.value);
+  };
+
+
+  return (
+    <>
+      <Input
+        name={""}
+        label={""}
+        value={userInputValue}
+        onChange={setUserInputValue}
+      />
+      <Button text="New Game" onClick={fetchNewNumber} />
+      <Button
+        text="Submit"
+        onClick={() => {
+          compareValues(number, userInputValue);
+        }}
+      />
+    </>
+  );
 };
 
 export default App;
