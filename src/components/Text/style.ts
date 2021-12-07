7c3e27b4e458aeb0a3d5998810be0924cf1c5edc
matheusifz <matheusifz@gmail.com>
2021-12-07 13:17:49 -0300

    feat: change text and number position

--- src/components/Text/style.ts
+++ src/components/Text/style.ts
@@ -7,6 +7,11 @@ export const Text = styled.h1`
   font-size: 36px;
   line-height: 44px;
   text-align: center;
+  border: 0;
+  padding: 0;
+  box-sizing: border-box;
+  display: flex;
+  justify-content: center;
   background-color: #f3ec78;
   background-image: linear-gradient(174.92deg, #ef6c00, #db6300);
   background-size: 100%;
